96680d12da7f571661d85e8044270d2ca7aa9cee
Harshita <harshitarera@gmail.com>
2025-09-06 15:26:13 +0530

    Final-commit

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import path from "path";
 
 import { connectDB } from "./db/connectDB.js";
 
@@ -12,7 +11,6 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
@@ -21,15 +19,8 @@ app.use(cookieParser()); // allows us to parse incoming cookies
 
 app.use("/api/auth", authRoutes);
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
-
-app.listen(PORT, () => {
+app.listen(PORT,() => {
 	connectDB();
-	console.log("Server is running on port: ", PORT);
+	console.log("Server is running on port:",PORT);
 });
